5913376890f8980590a36fa9b70589a33d688f11
Mariana Luciano <marianaluciano201@gmail.com>
2025-07-28 18:50:33 -0300

    subir para o netlify

--- projeto-academia/src/main/java/com/cefet/projeto_academia/config/SecurityConfig.java
+++ projeto-academia/src/main/java/com/cefet/projeto_academia/config/SecurityConfig.java
@@ -132,7 +132,7 @@ public class SecurityConfig {
 	      @Override
 	      public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	          .allowedOrigins("http://localhost:4200", "https://ds-guia12.netlify.app")
+	          .allowedOrigins("http://localhost:4200", "https://projetoacademia-ds.netlify.app/")
 	          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	          .allowedHeaders("*")
 	          .allowCredentials(true);
